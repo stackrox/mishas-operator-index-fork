b057e1a432bdc2d0a90c36cc9ff75d89ff29da37
Aleksandr Kurlov <akurlov@redhat.com>
2025-07-15 02:47:05 +0200

    Move files and address review comments

--- cmd/generate-catalog/generate_test.go
+++ cmd/generate-catalog/generate_test.go
@@ -113,7 +113,7 @@ func TestShouldBePersistentToMajorVersion(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			version := semver.MustParse(tt.version)
-			result := ShouldBePersistentToMajorVersion(version)
+			result := shouldBePersistentToMajorVersion(version)
 			assert.Equal(t, tt.expected, result)
 		})
 	}
